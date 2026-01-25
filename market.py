92839f5d25243d7bd8cd4aaf70ee20691c04450f
Elio Valentino Anci <e.anci@alumno.um.edu.ar>
2026-01-25 18:35:57 -0300

    add(market): finish data presentation.

--- market.py
+++ market.py
@@ -1,11 +1,51 @@
 import requests
-from statistics import median, std
+from statistics import median, stdev
+from json import dumps, loads
+from datetime import date
 
 url_catalog = 'https://static.compragamer.com/armado_pc_caracteristicas'
 url_products = 'https://static.compragamer.com/productos'
 
-def clean_mem(mem):
-    unused_keys = ['disipador','sodimm','w','slot','activo','latencia_cl','disipacion_alta','es_cudimm','marca','voltaje']
+def clean_mem(mem, secondary=False):
+    '''
+    Clean the unused keys on a memory dictionary.
+
+    Recieves a dictionary and remove hardcoded keys.
+        Optionally recieves secondary, that uses 
+        a secondary hardcoded list of unused keys.
+    '''
+    if secondary:
+        unused_keys = [
+                'precios_cuotas',
+                'imagenes',
+                'precioListaAnterior',
+                'precioEspecialAnterior',
+                'garantia',
+                'garantia_oficial',
+                'busca_en_ml',
+                'precioEspecialCombo',
+                'iva',
+                'impuesto_interno_importacion',
+                'precioListaCombo',
+                'id_nivel_falla',
+                'descripcion_falla',
+                'tipo_falla',
+                'promocion'
+            ]
+    else:
+        unused_keys = [
+                'disipador',
+                'sodimm',
+                'w',
+                'slot',
+                'activo',
+                'latencia_cl',
+                'disipacion_alta',
+                'es_cudimm',
+                'marca',
+                'voltaje'
+            ]
+
     for key in unused_keys:
         mem.pop(key)
 
@@ -35,7 +75,11 @@ def get_price():
     for mem in mems:
         clean_mem(mem) # return void. Here mem is passed by reference
         for product in product_mems:
-            if mem.get('id_producto') == product.get('id_producto'):
+            # The number 15 is to get the DIMM type of memory. Remove if want DIMM and SODIMM. Use 47 if just SODIMM
+            if mem.get('id_producto') == product.get('id_producto') and product.get('id_subcategoria') == 15:
+                # This shows the complete list shown of the page catalog
+                # clean_mem(product,True)
+                # print(dumps(product,indent=2))
                 mem['precio'] = product.get('precioEspecial')
                 mem['nombre'] = product.get('nombre')
                 tipo = mem.get('tipo')
@@ -55,7 +99,45 @@ def compute_stats(mems):
     prices = []
     for mem in mems:
         prices.append(mem.get('precio'))
+
+    return {'max':max(prices), 'min':min(prices), 'med':median(prices), 'std':round(stdev(prices),2)}
     
 
 if __name__ == '__main__':
     ddr416,ddr48,ddr516 = get_price()
+
+    stats = {}
+    stats['timestamp'] = str(date.today())
+
+    stats['DDR4 16GB'] = compute_stats(ddr416)
+    stats['DDR4 8GB'] = compute_stats(ddr48)
+    stats['DDR5 16GB'] = compute_stats(ddr516)
+
+    print(f'[+] RAM prices (based in compragamer.com)\n{dumps(stats,indent=2)}')
+
+    try:
+        open('prices.log','x')
+        print('[+] Log file created.')
+    except:
+        print('[+] Log file already exists.')
+
+    today_was_analized = False
+    with open('prices.log','r') as p:
+        try:
+            price_log = loads(p.read())
+            if stats.get('timestamp') != price_log[-1].get('timestamp'):
+                price_log.append(stats)
+            else:
+                today_was_analized = True
+        except:
+            price_log = []
+            price_log.append(stats)
+
+    if not today_was_analized:
+        with open('prices.log','w') as p:
+            p.write(dumps(price_log,indent=2))
+        print('[+] Written on log successfuly.')
+    else:
+        print(f'[+] The prices of today: {stats.get("timestamp")} are already written.')
+
+    print('[+] All done, Good Bye!')
